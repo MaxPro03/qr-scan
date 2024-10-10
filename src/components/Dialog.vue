9eea9f6bd864719dec669d9067c3cb45c919c443
unknown <pumpkinmaxpro@gmail.com>
2024-10-11 01:20:21 +0500

    updates

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -31,8 +31,7 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
+  transform: translate(-50%, -50%);
   background: rgba(0, 0, 0, 0.6);
   display: flex;
   justify-content: center;
