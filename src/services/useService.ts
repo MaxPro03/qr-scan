de25b7e30645c199c76100facaabb517b62461ed
@m.prokopenko <m.prokopenko@uzum.com>
2024-10-14 15:01:18 +0500

    updates

--- src/services/useService.ts
+++
@@ -1,7 +0,0 @@
-// services/userService.ts
-import apiClient from './api'
-
-export const fetchUsers = async () => {
-  const response = await apiClient.get('/users')
-  return response.data // Vue Query автоматически кеширует результат
-}
