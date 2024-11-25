a4e224602238244939bf20e15802e93d3144fb62
@m.prokopenko <m.prokopenko@uzum.com>
2024-11-25 18:38:21 +0500

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,62 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import {ref} from "vue";
-import { useTelegram } from './utils/composable/useTelegram.ts';
-import UiOrderCard from "@/components/order/UiOrderCard.vue";
-const { tg } = useTelegram();
+import { RouterView } from "vue-router"
+import { onMounted, ref } from "vue"
+import { useTelegram } from "./utils/composable/useTelegram.ts"
+import UiOrderCard from "@/components/order/UiOrderCard.vue"
 
-const userId = ref(tg?.initDataUnsafe?.user?.id);
-const username = ref(tg?.initDataUnsafe?.user?.username);
-const chatId = ref(tg?.initDataUnsafe?.chat?.id);
+const { tg } = useTelegram()
+
+const userId = ref(tg?.initDataUnsafe?.user?.id)
+const username = ref(tg?.initDataUnsafe?.user?.username)
+const chatId = ref(tg?.initDataUnsafe?.chat?.id)
 
 const orders = ref([
   {
     id: 123,
-    number: '725',
+    number: "725",
     status: 1,
     count: 12,
   },
   {
     id: 123,
-    number: '724',
+    number: "724",
     status: 1,
     count: 12,
   },
   {
     id: 123,
-    number: '723',
+    number: "723",
     status: 2,
     count: 12,
   },
   {
     id: 123,
-    number: '722',
+    number: "722",
     status: 2,
     count: 6,
   },
   {
     id: 123,
-    number: '721',
+    number: "721",
     status: 2,
     count: 3,
   },
 ])
+
+onMounted(() => {
+  tg?.lockOrientation()
+})
 </script>
 
 <template>
   <div id="app">
+    <div>
+      <!--          <button @click="tg?.addToHomeScreen()">Добавить на главный экран</button>-->
+      <p>User ID: {{ userId }}</p>
+      <p>Username: {{ username }}</p>
+      <p v-if="chatId">Chat ID: {{ chatId }}</p>
+      <p v-else>Chat ID не доступен</p>
+    </div>
     <RouterView />
   </div>
 </template>
--- src/views/QrScanView.vue
+++ src/views/QrScanView.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 import { QrcodeStream } from "vue-qrcode-reader"
 import { onMounted, ref } from "vue"
+import UiBadge from "@/components/order/UiBadge.vue"
+
+const modalContent = ref()
+const showModal = ref(false)
+const touchStartY = ref(0)
+const touchEndY = ref(0)
+const translateY = ref(0)
+const initialTranslateY = ref(0)
+const transitionStyle = ref("transform 0.3s ease-out") // Smooth transition
+
+const isSuccessMessage = ref(false)
+const isErrorMessage = ref(false)
+const result = ref(null)
+const errorMessage = ref("")
+const loading = ref(false)
 
 const selectedConstraints = ref({ facingMode: "environment" })
 const cameraFacingMode = ref("environment") // По умолчанию используем заднюю камеру
@@ -49,17 +64,16 @@ async function onCameraReady(capabilities) {
 }
 
 function onDetect(detectedCodes) {
-  console.log("detected", detectedCodes[0].rawValue)
+  console.log("detected", detectedCodes[0]?.rawValue)
   result.value = JSON.stringify(detectedCodes.map((code) => code.rawValue))
   // detectedCodes.map((code) => code.rawValue)
-  sendPostRequest(detectedCodes[0].rawValue)
+  sendPostRequest(detectedCodes[0]?.rawValue)
   // console.log('detected', result.value)
 }
 
 const sendPostRequest = async (id) => {
   loading.value = true
   errorMessage.value = ""
-  successMessage.value = ""
 
   try {
     const response = await fetch(
@@ -73,24 +87,98 @@ const sendPostRequest = async (id) => {
     )
 
     if (!response.ok) {
-      throw new Error("Ошибка. попробуйте снова")
+      throw new Error("Ошибка. Попробуйте снова")
     }
-
-    successMessage.value = "Успешно"
+    isSuccessMessage.value = true
+    openModal()
   } catch (error) {
+    isSuccessMessage.value = false
     errorMessage.value = error.message
+    openModal()
   } finally {
     loading.value = false
   }
 }
 
+function openModal() {
+  showModal.value = true
+  document.body.style.overflow = "hidden" // Disable body scroll
+}
+
+function closeModal() {
+  transitionStyle.value = "transform 0.2s ease-out" // Faster closing animation
+  translateY.value = window.innerHeight // Move the modal out of view
+  showModal.value = false
+  translateY.value = 0 // Reset translateY
+  transitionStyle.value = "transform 0.3s ease-out" // Restore smooth transition
+  document.body.style.overflow = "" // Enable body scroll
+  document.location.reload()
+}
+
+function handleOutsideClick(event) {
+  // Check if the click is outside the modal-content
+  if (event.target !== modalContent.value) {
+    closeModal()
+  }
+}
+
+function onTouchStart(event) {
+  touchStartY.value = event.touches[0].clientY
+  initialTranslateY.value = translateY.value
+}
+
+function onTouchMove(event) {
+  const touchCurrentY = event.touches[0].clientY
+  translateY.value = Math.max(
+    0,
+    initialTranslateY.value + touchCurrentY - touchStartY.value,
+  )
+}
+
+function onTouchEnd() {
+  if (translateY.value > 100) {
+    closeModal()
+  } else {
+    translateY.value = 0 // Reset to initial position if swipe is not sufficient
+  }
+}
+
 onMounted(() => {
   onCameraReady()
-  document.fullscreenElement
 })
 </script>
 
 <template>
+  <div v-if="showModal" class="overlay" @click="handleOutsideClick">
+    <div
+      class="modal-content"
+      ref="modalContent"
+      @touchstart="onTouchStart"
+      @touchmove="onTouchMove"
+      @touchend="onTouchEnd"
+      :style="{ transform: `translateY(${translateY}px)` }"
+    >
+      <span class="line"></span>
+      <h1 class="modal-content-title">Получение товара</h1>
+      <div
+        class="rounded-2xl bg-white p-10 flex justify-center"
+        v-if="isSuccessMessage"
+      >
+        <UiBadge color="green">Успешно</UiBadge>
+      </div>
+      <div class="rounded-2xl bg-white p-10 flex justify-center" v-else>
+        <UiBadge color="yellow">{{ errorMessage }}</UiBadge>
+      </div>
+      <!--      <div class="modal-content-description">-->
+      <!--        Лимит — это сумма, на которую вы<br />-->
+      <!--        можете покупать в рассрочку сейчас <br />и в будущем. Лимит уменьшается-->
+      <!--        после<br />-->
+      <!--        покупки и восстанавливается, когда вы<br />-->
+      <!--        вносите плановые платежи-->
+      <!--      </div>-->
+      <button @click="closeModal" class="modal-button">Закрыть</button>
+    </div>
+  </div>
   <div class="fullscreen bg-black pt-[50px] pb-[120px]">
     <div
       class="relative fullscreen mt-[50px] mb-[120px] rounded-[30px] overflow-hidden"
@@ -140,5 +228,72 @@ onMounted(() => {
   bottom: 0;
   right: 0;
   left: 0;
+}
+
+.overlay {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+}
+
+.modal-content {
+  background-color: #eef2f5;
+  width: 100%;
+  max-width: 500px;
+  height: 272px;
+  border-radius: 24px 24px 0 0;
+  padding: 16px;
+  position: relative;
+  touch-action: none; /* Prevent default scrolling */
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+}
+
+.line {
+  position: absolute;
+  top: 14px;
+  right: 50%;
+  transform: translateX(50%);
+  background-color: #c5c7ca;
+  width: 48px;
+  height: 5px;
+  border-radius: 7px;
+}
+
+.modal-content-title {
+  margin-top: 10px;
+  margin-bottom: 12px;
+  font-size: 18px;
+  line-height: 32px;
+  font-weight: 500;
+  text-align: center;
+}
+
+.modal-content-description {
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 500;
+  margin-bottom: auto;
+}
+
+.modal-button {
+  color: #212121;
+  padding: 12px;
+  border-radius: 7px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  border: 1px solid #cdcdcd;
+  margin: 20px 0 40px 0;
+  font-size: 16px;
+  font-weight: 600;
+  height: 48px;
 }
 </style>
